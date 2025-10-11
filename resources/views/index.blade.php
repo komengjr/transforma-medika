f50ad721d00146999ca249cb20401c6e50c17867
komengjr <47206605+komengjr@users.noreply.github.com>
2025-10-11 13:46:51 +0700

    updateselalus111

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -164,8 +164,8 @@
                             </div>
                         </li>
                         <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                                id="pagess">Pages</a>
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="pagess">Pages
+                            </a>
                             <div class="dropdown-menu dropdown-menu-card border-0 mt-0" aria-labelledby="pagess">
                                 <div class="card navbar-card-pages shadow-none dark__bg-1000">
                                     <div class="card-body scrollbar max-h-dropdown"><img class="img-dropdown"
@@ -658,11 +658,13 @@
                     <div class="col-md-11 col-lg-8 col-xl-4 pb-7 pb-xl-9 text-center text-xl-start"><a
                             class="btn btn-outline-danger mb-4 fs--1 border-2 rounded-pill" href="#!"><span class="me-2"
                                 role="img" aria-label="Gift">🎁</span>Become a pro</a>
-                        <h1 class="text-white fw-light">Bring <span class="typed-text fw-bold"
+                        <h1 class="text-white fw-light"><strong>Innoventra</strong> <small>Bring</small> <span
+                                class="typed-text fw-bold"
                                 data-typed-text='["design","beauty","elegance","perfection"]'></span><br />to your
                             webapp</h1>
-                        <p class="lead text-white opacity-75">With the power of Falcon, you can now focus only on
-                            functionaries for your digital products, while leaving the UI design on us!</p><a
+                        <p class="lead text-white opacity-75 text-justify">Lorem ipsum dolor sit amet consectetur
+                            adipisicing elit.
+                            Adipisci officiis at minus non atque id iure saepe ipsum, explicabo, optio quam!.!</p><a
                             class="btn btn-outline-light border-2 rounded-pill btn-lg mt-4 fs-0 py-2" href="#!">Start
                             building with the falcon<span class="fas fa-play ms-2"
                                 data-fa-transform="shrink-6 down-1"></span></a>
@@ -720,8 +722,9 @@
                 <div class="row justify-content-center text-center">
                     <div class="col-lg-8 col-xl-7 col-xxl-6">
                         <h1 class="fs-2 fs-sm-4 fs-md-5">WebApp theme of the future</h1>
-                        <p class="lead">Built on top of Bootstrap 5, super modular Falcon provides you gorgeous design
-                            &amp; streamlined UX for your WebApp.</p>
+                        <p class="lead">Built on top of Bootstrap 5, Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Ab, voluptate nam. Porro ipsum architecto doloremque consequatur sit labore asperiores
+                            ratione corporis, autem odit quae cumque, sed error nesciunt nihil excepturi!.</p>
                     </div>
                 </div>
                 <div class="row flex-center mt-8">
@@ -730,8 +733,9 @@
                     <div class="col-md col-lg-5 col-xl-4 mt-4 mt-md-0">
                         <h5 class="text-danger"><span class="far fa-lightbulb me-2"></span>PLAN</h5>
                         <h3>Blueprint &amp; design </h3>
-                        <p>With Falcon as your guide, now you have a fine-tuned state of the earth tool to make your
-                            wireframe a reality.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor corporis dicta nemo beatae
+                            sapiente architecto enim quam laborum quis qui veniam explicabo excepturi odit repudiandae,
+                            at numquam sint ullam dolorum..</p>
                     </div>
                 </div>
                 <div class="row flex-center mt-7">
@@ -740,8 +744,9 @@
                     <div class="col-md col-lg-5 col-xl-4 mt-4 mt-md-0">
                         <h5 class="text-info"> <span class="far fa-object-ungroup me-2"></span>BUILD</h5>
                         <h3>38 Sets of components</h3>
-                        <p>Build any UI effortlessly with Falcon's robust set of layouts, 38 sets of built-in elements,
-                            carefully chosen colors, typography, and css helpers.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quisquam inventore rem harum
+                            praesentium, maxime, ad dolore dolores assumenda id optio corrupti. Dolorem inventore soluta
+                            dolore, nostrum facilis ducimus nisi!.</p>
                     </div>
                 </div>
                 <div class="row flex-center mt-7">
@@ -750,8 +755,9 @@
                     <div class="col-md col-lg-5 col-xl-4 mt-4 mt-md-0">
                         <h5 class="text-success"><span class="far fa-paper-plane me-2"></span>DEPLOY</h5>
                         <h3>Review and test</h3>
-                        <p>From IE to iOS, rigorously tested and optimized Falcon will give the near perfect finishing
-                            to your webapp; from the landing page to the logout screen.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis unde numquam commodi
+                            assumenda molestias itaque expedita architecto consequatur corporis distinctio cum
+                            laudantium accusantium corrupti aspernatur excepturi, aliquid veritatis deserunt amet..</p>
                     </div>
                 </div>
             </div>
@@ -772,7 +778,7 @@
                 <div class="row">
                     <div class="col">
                         <h1 class="fs-2 fs-sm-4 fs-md-5">Here's what's in it for you</h1>
-                        <p class="lead">Things you will get right out of the box with Falcon.</p>
+                        <p class="lead">Things you will get right out of the box with Innoventra.</p>
                     </div>
                 </div>
                 <div class="row mt-6">
@@ -782,7 +788,7 @@
                             <div class="card-body pt-6 pb-4">
                                 <h5 class="mb-2">Bootstrap 5.x</h5>
                                 <p>Build your webapp with the world's most popular front-end component library along
-                                    with Falcon's 32 sets of carefully designed elements.</p>
+                                    with Innoventra's 32 sets of carefully designed elements.</p>
                             </div>
                         </div>
                     </div>
@@ -791,7 +797,7 @@
                             <div class="card-span-img"><span class="fab fa-node-js fs-5 text-success"></span></div>
                             <div class="card-body pt-6 pb-4">
                                 <h5 class="mb-2">SCSS &amp; Javascript files</h5>
-                                <p>With your purchased copy of Falcon, you will get all the uncompressed & documented
+                                <p>With your purchased copy of Innoventra, you will get all the uncompressed & documented
                                     SCSS and Javascript source code files.</p>
                             </div>
                         </div>
@@ -829,8 +835,8 @@
                             <div class="swiper-wrapper">
                                 <div class="swiper-slide">
                                     <div class="px-5 px-sm-6">
-                                        <p class="fs-sm-1 fs-md-2 fst-italic text-dark">Falcon is the best option if you
-                                            are looking for a theme built with Bootstrap. On top of that, Falcon&apos;s
+                                        <p class="fs-sm-1 fs-md-2 fst-italic text-dark">Innoventra is the best option if you
+                                            are looking for a theme built with Bootstrap. On top of that, Innoventra&apos;s
                                             creators and support staff are very brilliant and attentive to users&apos;
                                             needs.</p>
                                         <p class="fs-0 text-600">- Scott Tolinski, Web Developer</p><img
@@ -850,7 +856,7 @@
                                 </div>
                                 <div class="swiper-slide">
                                     <div class="px-5 px-sm-6">
-                                        <p class="fs-sm-1 fs-md-2 fst-italic text-dark">When I first saw Falcon, I was
+                                        <p class="fs-sm-1 fs-md-2 fst-italic text-dark">When I first saw Innoventra, I was
                                             totally blown away by the care taken in the interface. It felt like
                                             something that I&apos;d really want to use and something I could see being a
                                             true modern replacement to the current class of Bootstrap themes.</p>
@@ -915,7 +921,7 @@
                 <div class="row">
                     <div class="col-lg-4">
                         <h5 class="text-uppercase text-white opacity-85 mb-3">Our Mission</h5>
-                        <p class="text-600">Falcon enables front end developers to build custom streamlined user
+                        <p class="text-600">Innoventra enables front end developers to build custom streamlined user
                             interfaces in a matter of hours, while it gives backend developers all the UI elements they
                             need to develop their web app.And it's rich design can be easily integrated with backends
                             whether your app is based on ruby on rails, laravel, express or any other server side
@@ -974,7 +980,7 @@
                                         <p class="text-600 opacity-50">Dec 25 &bull; 2min read</p>
                                     </li>
                                     <li>
-                                        <h5 class="fs-0 mb-0"><a class="link-600" href="#!"> The New Falcon Theme</a>
+                                        <h5 class="fs-0 mb-0"><a class="link-600" href="#!"> The New Innoventra Theme</a>
                                         </h5>
                                         <p class="text-600 opacity-50">Dec 23 &bull; 10min read </p>
                                     </li>
@@ -1002,7 +1008,7 @@
                 <div class="container py-3">
                     <div class="row justify-content-between fs--1">
                         <div class="col-12 col-sm-auto text-center">
-                            <p class="mb-0 text-600 opacity-85">Thank you for creating with Falcon <span
+                            <p class="mb-0 text-600 opacity-85">Thank you for creating with Innoventra <span
                                     class="d-none d-sm-inline-block">| </span><br class="d-sm-none" /> 2021 &copy; <a
                                     class="text-white opacity-85" href="https://themewagon.com">Themewagon</a></p>
                         </div>
@@ -1026,7 +1032,7 @@
                     <div class="modal-header px-5 position-relative modal-shape-header bg-shape">
                         <div class="position-relative z-index-1 light">
                             <h4 class="mb-0 text-white" id="authentication-modal-label">Register</h4>
-                            <p class="fs--1 mb-0 text-white">Please create your free Falcon account</p>
+                            <p class="fs--1 mb-0 text-white">Please create your free Innoventra account</p>
                         </div>
                         <button class="btn-close btn-close-white position-absolute top-0 end-0 mt-2 me-2"
                             data-bs-dismiss="modal" aria-label="Close"></button>
@@ -1092,6 +1098,8 @@
     <script src="{{ asset('vendors/bootstrap/bootstrap.min.js') }}"></script>
     <script src="{{ asset('vendors/anchorjs/anchor.min.js') }}"></script>
     <script src="{{ asset('vendors/is/is.min.js') }}"></script>
+    <script src="{{ asset('vendors/swiper/swiper-bundle.min.js')}}"></script>
+    <script src="{{ asset('vendors/typed.js/typed.js')}}"></script>
     <script src="{{ asset('vendors/fontawesome/all.min.js') }}"></script>
     <script src="{{ asset('vendors/lodash/lodash.min.js') }}"></script>
     <script src="{{ asset('vendors/list.js/list.min.js') }}"></script>
