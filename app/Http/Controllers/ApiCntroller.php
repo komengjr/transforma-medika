a97a4e3492653779109a0282c7bf2d6e2867d19d
komengjr <47206605+komengjr@users.noreply.github.com>
2025-10-14 13:44:10 +0700

    updateselalu

---
+++ app/Http/Controllers/ApiCntroller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Facade\FlareClient\Http\Response;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ApiCntroller extends Controller
+{
+    public function data_product()
+    {
+        try {
+            $category = DB::table('log_m_product')->get();
+            return response()->json($category);
+        } catch (QueryException $e) {
+            $error = [
+                'error' => $e->getMessage()
+            ];
+            return response()->json($error, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
