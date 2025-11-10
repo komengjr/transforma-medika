2a0c1983962b2933780e592ab5e7d3d8dcd6a741
komengjr <47206605+komengjr@users.noreply.github.com>
2025-11-10 22:53:39 +0700

    updatecontact111211

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Movie;
+use Illuminate\Support\Facades\DB;
+
 class MovieController extends Controller
 {
     public function index()
     {
         $movies = Movie::latest()->paginate(12);
-        $moviee = Movie::get();
+        $moviee = DB::table('movies')->get();
         return view('boxoffice', compact('movies','moviee'));
     }
 
