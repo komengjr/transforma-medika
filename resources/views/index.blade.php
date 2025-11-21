fe557a12d599b57ef66eeae320763e77cb5e97dd
komengjr <47206605+komengjr@users.noreply.github.com>
2025-11-21 19:35:06 +0700

    updatesasd

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -58,7 +58,81 @@
 <!-- <section> close ============================-->
 <!-- ============================================-->
 
+<section class="bg-300 text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="fs-2 fs-sm-4 fs-md-5">Berita Untuk Anda</h1>
+                <p class="lead">Untuk mencapai visi tersebut, Innoventra Mempunyai Fitur Berita Terkini:</p>
+            </div>
+        </div>
+        <div class="card mb-3">
+            <div class="card-body">
+                <div class="row">
+                    @foreach ($data as $datas)
+                    <div class="mb-4 col-md-6 col-lg-4">
+                        <div class="border rounded-1 h-100 d-flex flex-column justify-content-between pb-3">
+                            <div class="overflow-hidden">
+                                <div class="position-relative rounded-top overflow-hidden">
+                                    <a class="d-block"
+                                        href="{{route('news_detail', ['id' => $datas->news_data_slug])}}"><img
+                                            class="img-fluid rounded-top" src="{{$datas->news_data_thumbnail}}"
+                                            alt=""></a><span
+                                        class="badge rounded-pill bg-success position-absolute mt-2 me-2 z-index-2 top-0 end-0">News</span>
+                                </div>
+                                <div class="p-3">
+                                    <h5 class="fs-0">
+                                        <a class="text-dark"
+                                            href="{{route('news_detail', ['id' => $datas->news_data_slug])}}">
+                                            {{$datas->news_data_title}}
+                                        </a>
+                                    </h5>
 
+                                    <h5 class="fs-md-2 text-warning mb-0 d-flex align-items-center mb-3">
+                                        <!-- $1199.5
+                                                                                <del class="ms-2 fs--1 text-500">$2399 </del> -->
+                                    </h5>
+                                    <p class="fs--1 mb-1 text-start">
+                                        @php
+                                            echo Str::limit($datas->news_data_content, 120);
+                                        @endphp
+                                    </p>
+                                    <p class="fs--1 mb-1 text-primary">
+                                        by {{$datas->news_data_author}}
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="d-flex flex-between-center px-3">
+                                <div class="text-warning">
+                                    @php
+                                    $view = DB::table('news_view')->where('news_data_code', $datas->news_data_code)->count();
+                                    @endphp
+                                    <span class="far fa-eye"></span>
+                                    <span class="ms-1"> {{ $view }} View </span>
+                                </div>
+                                <div>
+                                    <a class="btn btn-sm btn-falcon-default me-2"
+                                        href="{{route('news_detail', ['id' => $datas->news_data_slug])}}"
+                                        data-bs-toggle="tooltip" data-bs-placement="top" title=""
+                                        data-bs-original-title="Lihat Berita" aria-label="Lihat Berita">
+                                        <span class="fas fa-external-link-alt"></span> Baca Selengkapnya
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+
+                </div>
+            </div>
+            <div class="card-footer bg-light d-flex justify-content-center">
+
+            </div>
+        </div>
+    </div>
+    <!-- end of .container-->
+
+</section>
 
 
 <!-- ============================================-->
@@ -199,110 +273,7 @@
 </section>
 <!-- <section> close ============================-->
 <!-- ============================================-->
-<section class="bg-200 text-center">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1 class="fs-2 fs-sm-4 fs-md-5">Berita Untuk Anda</h1>
-                <p class="lead">Untuk mencapai visi tersebut, Innoventra Mempunyai Fitur Berita Terkini:</p>
-            </div>
-        </div>
-        <div class="card mb-3">
-            <div class="card-body">
-                <div class="row">
-                    @foreach ($data as $datas)
-                    <div class="mb-4 col-md-6 col-lg-4">
-                        <div class="border rounded-1 h-100 d-flex flex-column justify-content-between pb-3">
-                            <div class="overflow-hidden">
-                                <div class="position-relative rounded-top overflow-hidden">
-                                    <a class="d-block"
-                                        href="{{route('news_detail', ['id' => $datas->news_data_slug])}}"><img
-                                            class="img-fluid rounded-top" src="{{$datas->news_data_thumbnail}}"
-                                            alt=""></a><span
-                                        class="badge rounded-pill bg-success position-absolute mt-2 me-2 z-index-2 top-0 end-0">News</span>
-                                </div>
-                                <div class="p-3">
-                                    <h5 class="fs-0">
-                                        <a class="text-dark"
-                                            href="{{route('news_detail', ['id' => $datas->news_data_slug])}}">
-                                            {{$datas->news_data_title}}
-                                        </a>
-                                    </h5>
 
-                                    <h5 class="fs-md-2 text-warning mb-0 d-flex align-items-center mb-3">
-                                        <!-- $1199.5
-                                                                                <del class="ms-2 fs--1 text-500">$2399 </del> -->
-                                    </h5>
-                                    <p class="fs--1 mb-1">
-                                        by {{$datas->news_data_author}}
-                                    </p>
-                                    <p class="fs--1 mb-1">
-                                        News: <strong class="text-success">Available</strong>
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="d-flex flex-between-center px-3">
-                                <div>
-                                    @php
-                                    $view = DB::table('news_view')->where('news_data_code', $datas->news_data_code)->count();
-                                    @endphp
-                                    <span class="far fa-eye"></span>
-                                    <span class="ms-1"> {{ $view }} View </span>
-                                </div>
-                                <div>
-                                    <a class="btn btn-sm btn-falcon-default me-2"
-                                        href="{{route('news_detail', ['id' => $datas->news_data_slug])}}"
-                                        data-bs-toggle="tooltip" data-bs-placement="top" title=""
-                                        data-bs-original-title="Lihat Berita" aria-label="Lihat Berita">
-                                        <span class="fas fa-external-link-alt"></span> Show
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    @endforeach
-
-                </div>
-            </div>
-            <div class="card-footer bg-light d-flex justify-content-center">
-                <div>
-                    <button class="btn btn-falcon-default btn-sm me-2" type="button" disabled="disabled"
-                        data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Prev">
-                        <svg class="svg-inline--fa fa-chevron-left fa-w-10" aria-hidden="true" focusable="false"
-                            data-prefix="fas" data-icon="chevron-left" role="img" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 320 512" data-fa-i2svg="">
-                            <path fill="currentColor"
-                                d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z">
-                            </path>
-                        </svg><!-- <span class="fas fa-chevron-left"></span> Font Awesome fontawesome.com --></button><a
-                        class="btn btn-sm btn-falcon-default text-primary me-2" href="#!">1</a><a
-                        class="btn btn-sm btn-falcon-default me-2" href="#!">2</a><a
-                        class="btn btn-sm btn-falcon-default me-2" href="#!">
-                        <svg class="svg-inline--fa fa-ellipsis-h fa-w-16" aria-hidden="true" focusable="false"
-                            data-prefix="fas" data-icon="ellipsis-h" role="img" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 512 512" data-fa-i2svg="">
-                            <path fill="currentColor"
-                                d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z">
-                            </path>
-                        </svg><!-- <span class="fas fa-ellipsis-h"></span> Font Awesome fontawesome.com --></a><a
-                        class="btn btn-sm btn-falcon-default me-2" href="#!">35</a>
-                    <button class="btn btn-falcon-default btn-sm" type="button" data-bs-toggle="tooltip"
-                        data-bs-placement="top" title="" data-bs-original-title="Next">
-                        <svg class="svg-inline--fa fa-chevron-right fa-w-10" aria-hidden="true" focusable="false"
-                            data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 320 512" data-fa-i2svg="">
-                            <path fill="currentColor"
-                                d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z">
-                            </path>
-                        </svg><!-- <span class="fas fa-chevron-right"> </span> Font Awesome fontawesome.com -->
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- end of .container-->
-
-</section>
 
 
 @endsection
