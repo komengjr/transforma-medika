c06a997cc508bb9a0ca40db6326be775a014dde1
komengjr <47206605+komengjr@users.noreply.github.com>
2026-01-05 20:43:54 +0700

    updasdsasda111

--- app/Http/Controllers/Brodcast/BrodcastController.php
+++ app/Http/Controllers/Brodcast/BrodcastController.php
@@ -173,7 +173,7 @@ class BrodcastController extends Controller
                         $nomorhp = '+62' . substr($nomorhp, 1);
                     }
                 }
-                $text = "Halo *Bapak / Ibu* \n" . $request->subject . "\n\n" . $request->text . "\n\nSupport By. Innoverta";
+                $text = "Halo *Bapak / Ibu* \n" . $request->subject . "\n\n" . $request->text . "\n\nSupport By. Innoventra";
                 DB::table('v_log_whatsapp')->insert([
                     'v_log_whatsapp_code' => str::uuid(),
                     'd_reg_order_list_code' => str::uuid(),
