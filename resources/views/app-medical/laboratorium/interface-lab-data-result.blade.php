fa3d8210211fcb5b543c9fb6ad587e4643f524d6
komengjr <47206605+komengjr@users.noreply.github.com>
2026-08-01 10:42:10 +0700

    updateinterface1

--- resources/views/app-medical/laboratorium/interface-lab-data-result.blade.php
+++ resources/views/app-medical/laboratorium/interface-lab-data-result.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.layouts')
 @section('base.css')
-<!-- DataTables Bootstrap 5 CSS -->
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 @endsection
 @section('content')
@@ -82,8 +81,6 @@
 </div>
 
 <!-- SCRIPTS -->
-<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 
